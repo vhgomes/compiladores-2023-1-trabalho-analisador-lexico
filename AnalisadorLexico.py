79287bd1c76bd6a3ab600977fd6d8bf5912da61a
Victor Hugo <vhclaudiogomes@gmail.com>
2023-04-26 10:12:40 -0300

    Adicionado umas partes que faltava

--- AnalisadorLexico.py
+++ AnalisadorLexico.py
@@ -7,8 +7,10 @@ def tokenizar_codigo_fonte(codigo_fonte):
     padrao_espaco_branco = re.compile(r'\s+')
     padrao_identificador = re.compile(r'[a-zA-Z_]\w*')
     padrao_keyword = re.compile(r'\b(int|char|long|short|float|double|void|if|else|for|while|do|break|continue|struct|switch|case|default|return|printf)\b')
-    padrao_operadores = re.compile(r'(\+\+|--|\+=|-=|\*=|/=|%=|==|!=|<=|>=|&&|\|\||[;+\-*/%.,<>&|!^=~(){}])')
+    padrao_operadores = re.compile(r'(\+\+|--|\+=|-=|\*=|/=|%=|==|!=|<=|>=|&&|\|\||[;+\-*/%.,<>&|!^=~])')
     padrao_inteiros = re.compile(r'\d+')
+    padrao_ponto_flutuante = re.compile(r'(\d+\.\d+)')
+    padrao_delimitador = re.compile(r"\[|\]|\(|\)|\{|\}|\;|\,|\:")
     
     posicao = 0
     while posicao < len(codigo_fonte):
@@ -18,7 +20,7 @@ def tokenizar_codigo_fonte(codigo_fonte):
             posicao = match_espaco_branco.end()
             continue
         
-        
+
         match_identificador = padrao_identificador.match(codigo_fonte, posicao)
         if match_identificador:
             identificador = match_identificador.group()
@@ -30,26 +32,40 @@ def tokenizar_codigo_fonte(codigo_fonte):
                 tokens.append(('Identificador', identificador))
                 posicao += len(identificador)
             continue
+
+        match_delimitador = padrao_delimitador.match(codigo_fonte, posicao)
+        if match_delimitador:
+            tokens.append(('Delimitador', match_delimitador.group()))
+            posicao += len(match_delimitador.group())
+            continue
         
         match_operadores = padrao_operadores.match(codigo_fonte, posicao)
         if match_operadores:
             tokens.append(('Operadores', match_operadores.group()))
             posicao += len(match_operadores.group())
             continue
-        
+
+
+        match_ponto_flutuante = padrao_ponto_flutuante.match(codigo_fonte, posicao)
+        if match_ponto_flutuante:
+            tokens.append(('Ponto Flutuante', match_ponto_flutuante.group()))
+            posicao += len(match_ponto_flutuante.group())
+            continue
+
         match_inteiros = padrao_inteiros.match(codigo_fonte, posicao)
         if match_inteiros:
             tokens.append(('Inteiro', match_inteiros.group()))
             posicao += len(match_inteiros.group())
             continue
+
         
-        raise RuntimeError(f'Token não identificado na posição => {posicao}')
+        raise RuntimeError(f'Token não identificado na posição => {posicao} {codigo_fonte[posicao]}')
     
     return tokens
 
 
 if __name__ == '__main__':
-    with open('testes/teste1.c', 'r') as file:
+    with open('testes/teste5.c', 'r') as file:
         input = file.read()
         tokens = tokenizar_codigo_fonte(input)
         for token in tokens:
